62cb108f6434b05d234f7e24e8d3334467992836
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-03-28 13:36:13 -0300

    olhos reptile

--- resources/views/cards/eye.blade.php
+++ resources/views/cards/eye.blade.php
@@ -2,10 +2,10 @@
 <x-card class="plant" type="eye" card="confusion" energy="1" description="Embaralhe 1 {!!$confused!!} no baralho de descarte do oponente." />
 <x-card class="plant" type="eye" card="cucumberslive" energy="2" description="Cure seu time e aplique 5 {!!$healingboost!!}" />
 <x-card class="plant" type="eye" card="blossom" energy="0" description="Coloque 1 carta de Planta do seu baralho de compra para sua mão. {!!$banish!!}" />
-<x-card class="reptile" type="eye" card="gecko" energy="0" 0 description="" />
-<x-card class="reptile" type="eye" card="tricky" energy="1" description="" />
-<x-card class="reptile" type="eye" card="scar" energy="0" description="" />
-<x-card class="reptile" type="eye" card="topaz" energy="1" description="" />
+<x-card class="reptile" type="eye" card="gecko" energy="0" description="Esse Axie perde 50 de VIDA, Cura e {!!$cleanse!!} 1 desvantagem nos outros aliados." />
+<x-card class="reptile" type="eye" card="tricky" energy="1" description="Se {!!$initial!!} troque essa carta com 1 carta selecionada do teu baralha de banimento." />
+<x-card class="reptile" type="eye" card="scar" energy="0" description="Pegue 1 carta de Reptil do seu baralho de compra para sua mão. {!!$banish!!}" />
+<x-card class="reptile" type="eye" card="topaz" energy="1" description="Da 10 de DANO Bônus por carta banida." />
 <x-card class="bird" type="eye" card="mavis" energy="1" description="" />
 <x-card class="bird" type="eye" card="lucas" energy="0" description="" />
 <x-card class="bird" type="eye" card="littleowl eyes" energy="1" description="" />
