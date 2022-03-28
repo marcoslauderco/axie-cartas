d706e666df5935f765f9bda59b141cd0ef10c451
Marcos Lauder Correia de Oliveira <marcoslauder@maringa.gov.br>
2022-03-28 13:47:30 -0300

    olhos besta

--- resources/views/cards/eye.blade.php
+++ resources/views/cards/eye.blade.php
@@ -1,19 +1,19 @@
 <x-card class="plant" type="eye" card="papi" energy="1" description="Escolha qualquer aliado. Sempre que comprada, cura 20 de VIDA." />
 <x-card class="plant" type="eye" card="confusion" energy="1" description="Embaralhe 1 {!!$confused!!} no baralho de descarte do oponente." />
-<x-card class="plant" type="eye" card="cucumberslive" energy="2" description="Cure seu time e aplique 5 {!!$healingboost!!}" />
-<x-card class="plant" type="eye" card="blossom" energy="0" description="Coloque 1 carta de Planta do seu baralho de compra para sua mão. {!!$banish!!}" />
+<x-card class="plant" type="eye" card="cucumberslive" energy="2" description="Cure seu time e aplique 5 {!!$healingboost!!}." />
+<x-card class="plant" type="eye" card="blossom" energy="0" description="Coloque 1 carta de Planta do seu baralho de compra para sua mão. {!!$banish!!}." />
 <x-card class="reptile" type="eye" card="gecko" energy="0" description="Esse Axie perde 50 de VIDA, Cura e {!!$cleanse!!} 1 desvantagem nos outros aliados." />
 <x-card class="reptile" type="eye" card="tricky" energy="1" description="Se {!!$initial!!} troque essa carta com 1 carta selecionada do teu baralha de banimento." />
-<x-card class="reptile" type="eye" card="scar" energy="0" description="Pegue 1 carta de Reptil do seu baralho de compra para sua mão. {!!$banish!!}" />
+<x-card class="reptile" type="eye" card="scar" energy="0" description="Pegue 1 carta de Reptil do seu baralho de compra para sua mão. {!!$banish!!}." />
 <x-card class="reptile" type="eye" card="topaz" energy="1" description="Da 10 de DANO Bônus por carta banida." />
-<x-card class="bird" type="eye" card="mavis" energy="1" description="Invoca 1 {!!$mavis!!}" />
-<x-card class="bird" type="eye" card="lucas" energy="0" description="Pegue 1 carta de Pássaro do seu baralho de compra para sua mão. {!!$banish!!}" />
+<x-card class="bird" type="eye" card="mavis" energy="1" description="Invoca 1 {!!$mavis!!}." />
+<x-card class="bird" type="eye" card="lucas" energy="0" description="Pegue 1 carta de Pássaro do seu baralho de compra para sua mão. {!!$banish!!}." />
 <x-card class="bird" type="eye" card="littleowl eyes" energy="1" description="Todas as cartas Little Owl desse axie ganha 6 de DANO na batalha." />
 <x-card class="bird" type="eye" card="robin" energy="1" description="Invoca 1 {!!$littlerobin!!}" />
-<x-card class="beast" type="eye" card="zeal" energy="0" description="" />
-<x-card class="beast" type="eye" card="littlepeas" energy="1" description="" />
-<x-card class="beast" type="eye" card="puppy" energy="0" description="" />
-<x-card class="beast" type="eye" card="chubby" energy="2" description="" />
+<x-card class="beast" type="eye" card="zeal" energy="0" description="Pegue 1 carta de Besta do seu baralho de compra para sua mão. {!!$banish!!}." />
+<x-card class="beast" type="eye" card="littlepeas" energy="1" description="Aplica {!!$vulnerable!!} por 4 turnos" />
+<x-card class="beast" type="eye" card="puppy" energy="0" description="Pegue 1 carta de Besta do seu baralho de compra para sua mão. {!!$banish!!}." />
+<x-card class="beast" type="eye" card="chubby" energy="2" description="Ataca todos os inimigos." />
 <x-card class="bug" type="eye" card="bookworm" energy="0" description="" />
 <x-card class="bug" type="eye" card="nerdy" energy="0" description="" />
 <x-card class="bug" type="eye" card="neo" energy="1" description="" />
